1798bbc26dbcab6146d16722a5d2029e54ee3588
Moises Morales <moisesmorales1995@gmail.com>
2020-10-29 02:04:46 -0600

    Cambios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,50 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+
+bands.addBand(new Band("Soda Stereo"));
+bands.addBand(new Band("The Smiths"));
+bands.addBand(new Band("Megadeth"));
+bands.addBand(new Band("AC/DC"));
+bands.addBand(new Band("DLD"));
+
 
 // Mensajes de Sockets
 
-io.on('connection', client => {
+io.on('connection', client => {    
     console.log('Cliente Conectado');
+
+    client.emit('active-bands', bands.getBands() );
+
     client.on('disconnect', ()=> {
         console.log('Cliente Desconectado');
     });
 
-    client.on('mensaje',( payload )=>{
-        console.log("Mensaje!!!!!",payload);
-        io.emit("mensaje",{ admin: 'nuevo mensaje'} );
+    // client.on("emitir-mensaje", (payload)=>{
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+    // });
+	
+	// Permite agregar votos a las bandas
+    client.on('vote-band', (payload)=>{
+		bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
+
+	// Permite agregar nuevas bandas 
+	client.on('add-band', (payload)=>{
+		bands.addBand(new Band( payload.name ));
+        io.emit('active-bands', bands.getBands());
+	});
+	
+	// Permite eliminar una banda
+	client.on('delete-band', (payload)=>{
+		bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+	});
+	
+
 
 });
